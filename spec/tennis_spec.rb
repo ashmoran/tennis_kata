627ad3c427a34878053a1bae5196f75f5b0f0033
Ash Moran <ash.moran@patchspace.co.uk>
2012-10-22 10:59:13 +0100

    Game not started

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -3,7 +3,19 @@ require 'spec_helper'
 require 'tennis'
 
 describe Tennis do
-  it "does something" do
-    pending
+  subject(:tennis) { Tennis.new(self) }
+
+  def score_changed(new_score)
+    @score = new_score
+  end
+
+  before(:each) do
+    @score = :game_not_started
+  end
+
+  context "game not started" do
+    it "has not broadcast a score" do
+      expect(@score).to be == :game_not_started
+    end
   end
 end
