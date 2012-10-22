640ccda4ce53f0d00a65293fee23a9fd70d0adff
Ash Moran <ash.moran@patchspace.co.uk>
2012-10-22 11:06:48 +0100

    Game started

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,3 +1,9 @@
 class Tennis
+  def initialize(scoreboard)
+    @scoreboard = scoreboard
+  end
 
+  def start_game
+    @scoreboard.score_changed("0-0")
+  end
 end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -18,4 +18,11 @@ describe Tennis do
       expect(@score).to be == :game_not_started
     end
   end
+
+  context "game started" do
+    it "score starts at 0-0" do
+      tennis.start_game
+      expect(@score).to be == "0-0"
+    end
+  end
 end
