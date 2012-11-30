a6869896e07cf964ddaf1195730b0587efef3f5b
Ash Moran <ash.moran@patchspace.co.uk>
2012-11-30 23:22:59 +0000

    Simple scoring for Player 1, pre-win

--- lib/tennis_score_pairs.rb
+++ lib/tennis_score_pairs.rb
@@ -1,17 +1,35 @@
 class TennisScorePairs
+  TRANSITIONS = {
+    [0,  0]  => [15, 0],
+    [15, 0]  => [30, 0],
+    [30, 0]  => [40, 0]
+  }
+
   def initialize(scoreboard)
     @scoreboard = scoreboard
+    @score = [0, 0]
   end
 
   def start_game
-
+    score_changed
   end
 
   def point_to_player_a
-
+    @score = TRANSITIONS[@score]
+    score_changed
   end
 
   def point_to_player_b
 
+  end
+
+  private
+
+  def score_changed
+    @scoreboard.score_changed(humanize(@score))
+  end
+
+  def humanize(score)
+    "#{score[0]}-#{score[1]}"
   end
 end
